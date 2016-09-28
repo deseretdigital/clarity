2a36d9b74d59c1a42a7a8198587592269f9ab281
Matt Montgomery <mmontgomery@deseretdigital.com>
2016-09-28 15:12:43 -0600

    Update and rename StoryItemReleaseNotes.jsx to StoryItemReleaseNotes.react.jsx

---
+++ src/flux/components/Story/StoryItemReleaseNotes.react.jsx
@@ -0,0 +1,47 @@
+var _ = require('lodash');
+var React = require('react');
+var moment = require('moment');
+var classNames = require('classnames');
+
+var StoryQaBar = require('./StoryQaBar.react');
+
+var StoryItem = React.createClass({
+    statics: {
+        contexts: {
+            PULL_REQUEST: 'PULL_REQUEST',
+            MASTER: 'MASTER',
+            RELEASE: 'RELEASE'
+        }
+    },
+    _slugify: function (text) {
+        text = text.replace(/\ /g, '-');
+        text = text.replace(/\:/g, '');
+
+        return text;
+    },
+    _getLabels: function (labelsArr) {
+        return _.map(labelsArr, function (label) {
+            return label.name;
+        });
+    },
+    getDefaultProps: function () {
+        return {
+            context: 'PULL_REQUEST',
+            displayMode: 'standard'
+        }
+    },
+    render: function () {
+        return (
+          <tr>
+            <td className="story__title">
+                <a href={"https://www.pivotaltracker.com/story/show/" + this.props.id} target="_blank">#{this.props.id}</a>
+            </td>
+            <td>
+                {title}
+            </td>
+          </tr>
+        );
+    },
+});
+
+module.exports = StoryItem;
